c2c4b5c764bf6c3a4e421a1445aa82274f1c7fa1
정주원 <filocd05155@naver.com>
2025-11-05 18:37:01 +0900

    멤버 관련 파일 추가 및 수정

--- src/main/java/com/climbing/climbing_web/Entity/MemberEntity.java
+++ src/main/java/com/climbing/climbing_web/Entity/MemberEntity.java
@@ -1,4 +1,16 @@
 package com.climbing.climbing_web.Entity;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberEntity {
+    private Long id;
+    private String memberName;
+    private String memberEmail;
+    private String memberPassword;
 }
--- src/main/java/com/climbing/climbing_web/service/MemberService.java
+++ src/main/java/com/climbing/climbing_web/service/MemberService.java
@@ -1,13 +1,29 @@
 package com.climbing.climbing_web.service;
 
+import com.climbing.climbing_web.Entity.MemberEntity;
 import com.climbing.climbing_web.dto.MemberDTO;
+import com.climbing.climbing_web.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    public void save(MemberDTO memberDTO){
 
+    // MemberRepository 주입
+    private final MemberRepository memberRepository;
+
+    public void save(MemberDTO memberDTO){
+        // 1. DTO를 Entity로 변환 (DB에 저장하기 위함)
+        // DTO의 필드를 Entity의 생성자에 맞춰 주입
+        MemberEntity memberEntity = new MemberEntity(
+                null, // ID는 DB에서 자동 생성되므로 null
+                memberDTO.getMemberName(),
+                memberDTO.getMemberEmail(),
+                memberDTO.getMemberPassword()
+        );
+
+        // 2. Repository를 통해 DB에 저장
+        memberRepository.save(memberEntity);
     }
 }
