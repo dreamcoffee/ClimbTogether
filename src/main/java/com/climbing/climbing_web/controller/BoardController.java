075eb9869abc9f8f66463380dd660eb78feff91d
정주원 <filocd05155@naver.com>
2025-11-10 14:27:35 +0900

    게시글 작성 시 로그인 여부 확인

--- src/main/java/com/climbing/climbing_web/controller/BoardController.java
+++ src/main/java/com/climbing/climbing_web/controller/BoardController.java
@@ -2,13 +2,17 @@ package com.climbing.climbing_web.controller;
 
 import com.climbing.climbing_web.dto.BoardDTO;
 import com.climbing.climbing_web.service.BoardService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 
@@ -19,6 +23,21 @@ public class BoardController {
     public String getlist(Model model){
         List<BoardDTO> boardDTOList = boardservice.getList();
         model.addAttribute("boardList", boardDTOList);
+        return "community";
+    }
+
+    @GetMapping("/addPost")
+    public String addPost(HttpSession session){
+        if (session.getAttribute("loginId") == null){
+            log.warn("로그인 하지 않은 사용자 접근 시도");
+            return "redirect:/login";
+        }
+
+        return "addPost";
+    }
+
+    @PostMapping("/addPost")
+    public String addPost(BoardDTO boardDTO){
         return "community";
     }
 }
