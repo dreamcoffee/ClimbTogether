7b2196f837c0f9d25d6023d79139ee68351bf313
정주원 <filocd05155@naver.com>
2025-11-10 22:20:59 +0900

    글 쓰기 추가

--- src/main/java/com/climbing/climbing_web/service/BoardService.java
+++ src/main/java/com/climbing/climbing_web/service/BoardService.java
@@ -3,10 +3,12 @@ package com.climbing.climbing_web.service;
 import com.climbing.climbing_web.dto.BoardDTO;
 import com.climbing.climbing_web.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BoardService {
@@ -14,5 +16,10 @@ public class BoardService {
 
     public List<BoardDTO> getList(){
         return boardRepository.getList();
+    }
+
+    public void savePost(BoardDTO boardDTO){
+        log.info("[로그] BoardDTO : " + boardDTO);
+        boardRepository.save(boardDTO);
     }
 }
