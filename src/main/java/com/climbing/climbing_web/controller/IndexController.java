9446a2686ebb899eba798814d3cac01dc88610db
정주원 <filocd05155@naver.com>
2025-11-10 16:50:51 +0900

    log 삭제

--- src/main/java/com/climbing/climbing_web/controller/IndexController.java
+++ src/main/java/com/climbing/climbing_web/controller/IndexController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        log.info("index 메서드 call");
         return "index";
     }
 }
